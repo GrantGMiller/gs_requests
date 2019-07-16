134d7611e2ef8943c9677acca3a17b4acd9a9309
gmiller <gmiller@extron.com>
2019-07-16 14:42:37 -0400

    init commit

---
+++ gs_requests.py
@@ -0,0 +1,59 @@
+import urllib.request
+import json
+
+
+class HTTPSession:
+    def __init__(self):
+        cookieHandler = urllib.request.HTTPCookieProcessor()
+        self._opener = urllib.request.build_opener(cookieHandler)
+
+        self._proxyAddress = None
+        self._proxyPort = None
+
+    def get(self, url, data=None, proxies=None):
+        if proxies:
+            proxyString = list(proxies.values())[0]
+            self._proxyAddress = proxyString.split(':')[0].split('://')[-1]
+            try:
+                self._proxyPort = int(proxyString.split(':')[1])
+            except:
+                self._proxyPort = None
+
+            proxyHandler = urllib.request.ProxyHandler({
+                'http': 'http://{}:{}'.format(
+                    self._proxyAddress,
+                    self._proxyPort or '3128',  # default proxies port is 3128
+                ),
+                'https': 'https://{}:{}'.format(
+                    self._proxyAddress,
+                    self._proxyPort or '3128',  # default proxies port is 3128
+                ),
+            })
+            self._opener.add_handler(proxyHandler)
+
+        resp = self._opener.open(url, data=data)
+        return Response(raw=resp.read())
+
+    def auth(self, username, password):
+        authHandler = urllib.request.HTTPBasicAuthHandler()
+        authHandler.add_password(None, '/', username, password)
+        self._opener.add_handler(authHandler)
+
+
+class Response:
+    def __init__(self, raw):
+        self._raw = raw
+
+    @property
+    def raw(self):
+        return self._raw
+
+    @property
+    def json(self):
+        return json.loads(self._raw)
+
+    @property
+    def text(self):
+        return self._raw.decode()
+
+
---
+++ main.py
@@ -0,0 +1,17 @@
+from gs_requests import HTTPSession
+
+session = HTTPSession()
+
+session.auth('username', 'password')
+
+resp = session.get('http://www.extron.com', proxies={
+    'http': '172.17.16.79:3128',
+    'https': '172.17.16.79:3128',
+})
+
+print('resp.text=', resp.text)
+try:
+    print('resp.json=', resp.json)
+except Exception as e:
+    print(e)
+print('resp.raw=', resp.raw)
